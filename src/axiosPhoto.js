435a2a4cfd3eae95c159945ece3807cc2cbce7c9
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-08-27 17:35:21 +0300

    add start files

---
+++ src/axiosPhoto.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { page, per_page } from '.';
+const API_KEY = '29525266-43f22ff86b92049909965975c';
+
+const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=`;
+const FILTER_RESPONSE = `&orientation=horizontal&image_type=photo&safesearch=true`;
+
+export async function getPhoto(name) {
+  const params = new URLSearchParams({
+    page,
+    per_page,
+  });
+  let resolve = await axios.get(
+    `${BASE_URL}${name}${FILTER_RESPONSE}${params}`
+  );
+  let data = resolve.data;
+  return data;
+}
---
+++ src/index.js
@@ -0,0 +1,105 @@
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getPhoto } from './axiosPhoto';
+
+export const page = 1;
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  galleryBox: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+refs.form.addEventListener('submit', onSearchImage);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onLoadMore() {
+  clearPage();
+  const inputValue = '';
+  request(inputValue);
+}
+
+async function onSearchImage(e) {
+  e.preventDefault();
+
+  clearPage();
+  // page = 1;
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.searchQuery.value.trim();
+  inputValue = searchQuery;
+
+  if (!searchQuery) {
+    return clearPage();
+  }
+
+  request(searchQuery);
+
+  e.currentTarget.reset();
+
+  setTimeout(() => {
+    refs.loadMoreBtn.classList.add('show-button');
+  }, 2000);
+}
+
+function request(r) {
+  try {
+    getPhoto(r).then(renderPhoto);
+  } catch (onError) {
+    console.error(error);
+  }
+}
+
+function onError() {
+  Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function clearPage() {
+  refs.galleryBox.innerHTML = '';
+}
+
+function renderPhoto(data) {
+  makeGallary(data);
+}
+
+function makeGallary(data) {
+  const imageArgs = data.hits;
+  if (imageArgs.length === 0) {
+    onError();
+    return;
+  }
+  const markup = imageArgs.reduce(
+    (acc, image) =>
+      acc +
+      `<div class="photo-card">
+      <a class = "gallary__item" href="${image.largeImageURL}">
+      <img class ="gallery__image"  src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: </b>${image.likes}
+      </p>
+      <p class="info-item">
+        <b>Views: </b>${image.views}
+      </p>
+      <p class="info-item">
+        <b>Comments: </b>${image.comments}
+      </p>
+      <p class="info-item">
+        <b>Dowloads: </b>${image.downloads}
+      </p>
+    </div>
+  </div>`,
+    ''
+  );
+
+  refs.galleryBox.innerHTML = markup;
+  createLiteBox();
+}
+
+function createLiteBox() {
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.refresh;
+}
